8f3886e32baffa7ef3a60a33988d0fb3ca357597
qinSer <qinSer@123456>
2021-05-10 21:34:02 +0800

    first commit

---
+++ src/main/java/com/qin/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.qin.git;
+
+/**
+ * @author shkStart
+ * @create 2021-05-10 21:30
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
